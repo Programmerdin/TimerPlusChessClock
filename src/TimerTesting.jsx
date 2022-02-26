efaee6f84757138945165f9e36ef1d6200791d76
Programmerdin <testingemailodin@gmail.com>
2022-02-26 17:21:28 +0900

    TimerTesting works, can now make TimerTesting.jsx the new Timer.jsx from here on

--- src/TimerTesting.jsx
+++ src/TimerTesting.jsx
@@ -10,18 +10,20 @@ const TimerTesting = () => {
   const [timer2,setTimer2] = useState(0);
   const [timer1On,setTimer1On] = useState(false);
   const [timer2On,setTimer2On] = useState(false);
-  //turn timers on/off
-  const handleSetTimer1On = () => {
+  //turn timers on and update output states
+  const handleSetTimer1OnAndSetOutput1State = () => {
     setTimer1On(true);
+    setOutput1State(Level1FormatToPureSeconds(rawToLevel1Format(user_input1_state)));
   };
-  const handleSetTimer1Off = () => {
+  //turn timers off and update user_input_states to level2format(output-timer)
+  const handleSetTimer1OffAndSetUserInput1State = () => {
     setTimer1On(false);
+    setUserInput1State(PureSecondsToLevel2Format(output1_state-timer1));
   };
 
   //set output useState
   const [output1_state,setOutput1State] = useState(0);
   const [output2_state,setOutput2State] = useState(0);
-
 
   //Reset
   const ResetTimer1and2 = () => {
@@ -31,12 +33,6 @@ const TimerTesting = () => {
     setTimer2On(false);
   };
 
-  //update both input and output state as user types
-  const handleInput1AndOutput1States = (user_input) => {
-    setUserInput1State(user_input);
-    setOutput1State(user_input);
-  };
-  
   //1.user types pure nums in
   //2.turn it into formatted nums within the input field 
   //3.take the formatted nums and unformat it into pure seconds 
@@ -45,6 +41,7 @@ const TimerTesting = () => {
   //4.whenever pause is pressed
   //take seconds left and convert it into perf formatted nums 
   //then update it to user input state 
+  //5.display user_input_state when timer is false, display Level2format(output-timer) when timer is true 
   
   //rawnums: raw input
   //level1Format: formatted to be time but not correcting 66s into 1m6s
@@ -93,13 +90,51 @@ const TimerTesting = () => {
 
     //below 2 lines for for loop that will add up all the seconds from each digit of cleaned lvl1 formatted time
     let time_in_pure_seconds = 0;
-    const time_multiply_factor_array = [1,10,60,600,3600,36000,360000,3600000];
+    const time_multiply_factor_array = [1,10,60,600,3600,36000,360000,3600000,36000000];
 
     for (let i=length_of_cleaned_level1_formatted_time; i>0; i--){
       time_in_pure_seconds = time_in_pure_seconds + (Number(cleaned_level1_formatted_time.charAt(i-1))*Number(time_multiply_factor_array[length_of_cleaned_level1_formatted_time-i]));
       // //get an array 1,10,60,600,3600,36000 and look up what to times it by based on i
       // //add the (specific digit x value from array) to time in pure seconds
-      console.log(time_in_pure_seconds);
+    };
+    return time_in_pure_seconds;
+  };
+
+  function PureSecondsToLevel2Format(time_in_seconds){
+    let hours_no_remainder = Math.abs(Math.floor(time_in_seconds/3600));
+    let hours_remainder_in_seconds = Math.abs(time_in_seconds%3600);
+    let minutes_no_remainder = Math.abs(Math.floor(hours_remainder_in_seconds/60));
+    let seconds = Math.abs(hours_remainder_in_seconds%60);
+    
+    if(time_in_seconds<-3600){
+      return(
+        `${hours_no_remainder} ${("0"+minutes_no_remainder).slice(-2)} ${Math.floor(seconds)}`
+      );
+    } else
+    if (time_in_seconds<-60) {
+      return(
+        `${minutes_no_remainder} ${("0"+Math.floor(seconds)).slice(-2)}`
+      );
+    } else
+    if (time_in_seconds===0) {
+      return(
+        `${seconds.toFixed(0)}`
+      );
+    } else
+    if (time_in_seconds<60) {
+      return(
+        `${seconds.toFixed(0)}`
+      );
+    } else
+    if (time_in_seconds<3600) {
+      return(
+        `${minutes_no_remainder} ${("0"+Math.floor(seconds)).slice(-2)}`
+      );
+    } else
+    if (time_in_seconds>=3600) {
+      return(
+        `${hours_no_remainder} ${("0"+minutes_no_remainder).slice(-2)} ${("0"+Math.floor(seconds)).slice(-2)}`
+      );
     };
   };
 
@@ -136,13 +171,14 @@ const TimerTesting = () => {
         onChange={(e)=>handleRawToLevel1Format(e)}
         value={user_input1_state}
       ></input>
-      <button onClick={handleSetTimer1On}>Run</button>
-      <button onClick={handleSetTimer1Off}>Pause</button>
+      <button onClick={handleSetTimer1OnAndSetOutput1State}>Run</button>
+      <button onClick={handleSetTimer1OffAndSetUserInput1State}>Pause</button>
       <button onClick={ResetTimer1and2}>Reset</button>
       <p>user_input1_state:{user_input1_state}</p>
       <p>output1_state:{output1_state}</p>
       <p>timer1:{timer1}</p>
       <p>output1-timer1:{output1_state-timer1}</p>
+      <p>Level2Format(output1-timer1):{PureSecondsToLevel2Format(output1_state-timer1)}</p>
     </div>
   );
 };
